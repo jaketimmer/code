af6e760a557e87d1870174b68fcc69360ac3ccb2
Jake Timmer <jaketimmer14@gmail.com>
2014-02-02 23:20:27 -0500

    Finish static pages

--- rails_projects/sample_app/app/controllers/static_pages_controller.rb
+++ rails_projects/sample_app/app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  # In typical ruby these do nothing, the inheritance of ApplicationController is specific to rails
+  
   def home
   end
 
   def help
+  end
+
+  def about
   end
 end
